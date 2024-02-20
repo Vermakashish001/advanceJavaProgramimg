8c167d43401d682f811790816fee3ba47d581af0
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-02-20 16:48:57 +0530

    Commit

--- CheckBoxProject.java
+++ CheckBoxProject.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CheckBoxProject{
     public static void main(String[] args){
@@ -13,17 +15,17 @@ public class CheckBoxProject{
         Font f = new Font("Arial",Font.BOLD,34); 
 
         JCheckBox ckb1=new JCheckBox("Pizza - 250");
-        ckb1.setBounds(150,200,300,50); 
+        ckb1.setBounds(150,200,300,50);
         ckb1.setFont(f);
         c.add(ckb1);
 
         JCheckBox ckb2=new JCheckBox("Burger - 450");
-        ckb2.setBounds(150,250,300,50); 
+        ckb2.setBounds(150,250,300,50);
         ckb2.setFont(f);
         c.add(ckb2);
 
         JCheckBox ckb3=new JCheckBox("Momos - 350");
-        ckb3.setBounds(150,300,300,50); 
+        ckb3.setBounds(150,300,300,50);
         ckb3.setFont(f);
         c.add(ckb3);
 
@@ -33,9 +35,33 @@ public class CheckBoxProject{
         c.add(ckb4);
 
         JButton btn = new JButton("Bill Amount");
-        btn.setBounds(150,400,250,45);
+        btn.setBounds(150,400,300,50);
         btn.setFont(f);
         c.add(btn);
+
+        JLabel totalAmount = new JLabel("Total Amount: ");
+        totalAmount.setBounds(150,450,300,50);
+        totalAmount.setFont(f);
+        c.add(totalAmount);
+
+        btn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int total = 0;
+                if (ckb1.isSelected()) {
+                    total += 250;
+                }
+                if (ckb2.isSelected()) {
+                    total += 450;
+                }
+                if (ckb3.isSelected()) {
+                    total += 350;
+                }
+                if (ckb4.isSelected()) {
+                    total += 150;
+                }
+                totalAmount.setText("Total Amount: " + total);
+            }
+        });
 
         jf.setVisible(true);
     }
