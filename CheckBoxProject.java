0081efec33802adc6af523abd263ef7abfa7bd50
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-02-20 16:45:06 +0530

    commit

--- CheckBoxProject.java
+++ CheckBoxProject.java
@@ -13,25 +13,29 @@ public class CheckBoxProject{
         Font f = new Font("Arial",Font.BOLD,34); 
 
         JCheckBox ckb1=new JCheckBox("Pizza - 250");
-        ckb1.setBounds(150,200,300,50); // Increased width
+        ckb1.setBounds(150,200,300,50); 
         ckb1.setFont(f);
-        ckb1.setSelected(true); // This will make checkbox 1 selected by default
         c.add(ckb1);
 
         JCheckBox ckb2=new JCheckBox("Burger - 450");
-        ckb2.setBounds(150,250,300,50); // Increased width
+        ckb2.setBounds(150,250,300,50); 
         ckb2.setFont(f);
         c.add(ckb2);
 
         JCheckBox ckb3=new JCheckBox("Momos - 350");
-        ckb3.setBounds(150,300,300,50); // Increased width
+        ckb3.setBounds(150,300,300,50); 
         ckb3.setFont(f);
         c.add(ckb3);
 
         JCheckBox ckb4=new JCheckBox("Noodles - 150");
-        ckb4.setBounds(150,350,300,50); // Increased width
+        ckb4.setBounds(150,350,300,50);
         ckb4.setFont(f);
         c.add(ckb4);
+
+        JButton btn = new JButton("Bill Amount");
+        btn.setBounds(150,400,250,45);
+        btn.setFont(f);
+        c.add(btn);
 
         jf.setVisible(true);
     }
