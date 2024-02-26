68722ad57d3b97545f5b49342c1ae605b10725d2
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-02-26 16:03:51 +0530

    Commit

--- scrollPane.java
+++ scrollPane.java
@@ -10,12 +10,18 @@ public class scrollPane{
         jf.setLayout(new FlowLayout());
         
         JTextArea ta = new JTextArea(5,20);
-        ta.setBounds(100,100,300,50);
-        ta.setFont(new Font("A",Font.BOLD,34));
+        ta.setFont(new Font("Arial",Font.BOLD,34));
 
         JScrollPane jsp = new JScrollPane(ta, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        
         jf.add(jsp);
+
+        String []s={
+            "tea","samosha","momos","pizza","coffee","Noodles","Burger","Rice","Panner","chicken","fish"
+        };
+        JList<String>list=new JList<>(s);
+        JScrollPane jsp2 = new JScrollPane(list, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        jf.add(jsp2);
+        jsp2.setBounds(500,500,200,50);
 
         jf.setVisible(true);
     }
