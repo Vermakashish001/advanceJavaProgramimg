76f9639f60688e456a011cfe978edcd8315d1107
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-03-05 16:33:00 +0530

    Commit

---
+++ BoderLayoutDemo.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class BoderLayoutDemo{
+    public static void main(String[] args) {
+        JFrame jf = new JFrame("Border Layout");
+        jf.setBounds(100, 100, 800, 600);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        jf.setLocationRelativeTo(null);
+        Container c = jf.getContentPane();
+
+        BorderLayout bl = new BorderLayout();
+        c.setLayout(bl);
+        JButton b1 = new JButton("North");
+        JButton b2 = new JButton("South");
+        JButton b3 = new JButton("East");
+        JButton b4 = new JButton("West");
+        JButton b5 = new JButton("Center");
+
+        c.add(b1, BorderLayout.NORTH);
+        c.add(b2, BorderLayout.SOUTH);
+        c.add(b3, BorderLayout.EAST);
+        c.add(b4, BorderLayout.WEST);
+        c.add(b5, BorderLayout.CENTER);
+
+        Font f = new Font("Arial", Font.PLAIN, 16); // Define the font here
+        // Set the font for button b1
+
+        jf.setVisible(true);
+    }
+}
